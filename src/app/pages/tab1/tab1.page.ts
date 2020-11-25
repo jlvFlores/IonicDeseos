257865da9764b053645a37b9bfc98c33e57813cf
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-25 10:53:15 -0600

    129 Editar elementos de otras listas

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
 import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
@@ -55,5 +56,11 @@ export class Tab1Page {
     });
 
     alert.present();
+  }
+
+  listaSeleccionanda( lista: Lista) {
+
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
+
   }
 }
