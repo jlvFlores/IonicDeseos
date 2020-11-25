7e71caf9f3d07b553048fbd896a998a2d62f1867
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-25 11:30:51 -0600

    132 Eliminar una lista

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -25,6 +25,11 @@ export class DeseosService {
 
   }
 
+  borrarLista( lista: Lista ) {
+    this.listas = this.listas.filter( listaData => listaData.id !== lista.id );
+    this.guardarStorage();
+  }
+
   obtenerLista( id: string | number) {
     id = Number(id);
 
